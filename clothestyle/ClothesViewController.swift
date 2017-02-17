08bf634df395dcb8e57992f4841d8fd18542381b
Napatsorn Lam Sakulsuwan <PATCHUNG@Napatsorns-MacBook-Air.local>
2017-02-17 19:35:41 +0700

    start core data revisited

---
+++ clothestyle/ClothesViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ClothesViewController.swift
+//  clothestyle
+//
+//  Created by Napatsorn Lam Sakulsuwan on 2/16/17.
+//  Copyright © 2017 Napatsorn Lam Sakulsuwan. All rights reserved.
+//
+
+import UIKit
+
+class ClothesViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var clothesImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        clothesImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+  
+    @IBAction func AddTapped(_ sender: Any) {
+    }
+
+}
