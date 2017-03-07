7ae5dd2cc73397d0d27ff84ce163a6f761b2aec1
Napatsorn Lam Sakulsuwan <PATCHUNG@Napatsorns-MacBook-Air.local>
2017-03-07 18:13:12 +0700

    We can create game objects and them out of core data

--- clothestyle/ClothesViewController.swift
+++ clothestyle/ClothesViewController.swift
@@ -9,18 +9,24 @@
 import UIKit
 
 class ClothesViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var clothesImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     
     var imagePicker = UIImagePickerController()
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         imagePicker.delegate = self
+        
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -29,20 +35,28 @@ class ClothesViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBAction func cameraTapped(_ sender: Any) {
         
     }
-
+    
     @IBAction func photosTapped(_ sender: Any) {
         
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
     
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         clothesImageView.image = image
         imagePicker.dismiss(animated: true, completion: nil)
     }
-  
+    
     @IBAction func AddTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let clothes = Clothes(context: context)
+        clothes.title = titleTextField.text
+        clothes.image = UIImagePNGRepresentation(clothesImageView.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
-
+    
 }
--- clothestyle/ViewController.swift
+++ clothestyle/ViewController.swift
@@ -1,25 +1,53 @@
 //
-//  ViewController.swift
+//  ViewController1.swift
 //  clothestyle
 //
-//  Created by Napatsorn Lam Sakulsuwan on 2/16/17.
+//  Created by Napatsorn Lam Sakulsuwan on 3/7/17.
 //  Copyright © 2017 Napatsorn Lam Sakulsuwan. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var clothes : [Clothes] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Do ay additioal setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            clothes = try context.fetch(Clothes.fetchRequest())
+            tableView.reloadData()
+        }catch {
+            
+        }
+        
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clothes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let cloth = clothes[indexPath.row]
+        cell.textLabel?.text = cloth.title
+        cell.imageView?.image = UIImage(data : cloth.image as! Data)
+        return cell
+        navigationController!.popViewController(animated: true)
+    }
+    
+    
 }
+
+
 
